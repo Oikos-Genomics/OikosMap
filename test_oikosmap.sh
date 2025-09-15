f9b7355d12ee70bf9566abe8f5a14809bbb52c41
Keiler Collier <82856657+BirdmanRidesAgain@users.noreply.github.com>
2025-09-15 15:32:45 +0400

    Update test_oikosmap.sh
    Removed remaining instances of --indlist arg.

--- test_oikosmap.sh
+++ test_oikosmap.sh
@@ -11,4 +11,4 @@ eval "$(conda shell.bash hook)"
 ####################################################################################
 ### MAIN
 # Run program
-nextflow run OikosMap.nf -stub-run --indlist ./testdata/indlist.txt --indir ./testdata/readfiles --refseq ./testdata/refseq/Chlamydotis_macqueenii_Ctg1_15kbp.fa --prefix test #-with-trace -with-report
+nextflow run OikosMap.nf --indir ./testdata/readfiles --refseq ./testdata/refseq/Chlamydotis_macqueenii_Ctg1_15kbp.fa --prefix test -with-trace -with-report
